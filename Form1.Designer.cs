a85e929b4e8b27fbc9ad3b09480a6ce7bdf28373
Sam Mckee <samm@hortonvillerobotics.com>
2021-10-17 18:58:43 -0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace DadProgram
+{
+    partial class mainWindowFrm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createBtn = new System.Windows.Forms.Button();
+            this.odlbl = new System.Windows.Forms.Label();
+            this.odTxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.idTxtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.thicknessTxtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.creatorTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // createBtn
+            // 
+            this.createBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.createBtn.Location = new System.Drawing.Point(115, 183);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(75, 23);
+            this.createBtn.TabIndex = 0;
+            this.createBtn.Text = "Create";
+            this.createBtn.UseVisualStyleBackColor = false;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // odlbl
+            // 
+            this.odlbl.AutoSize = true;
+            this.odlbl.Location = new System.Drawing.Point(38, 27);
+            this.odlbl.Name = "odlbl";
+            this.odlbl.Size = new System.Drawing.Size(24, 15);
+            this.odlbl.TabIndex = 1;
+            this.odlbl.Text = "OD";
+            this.odlbl.Click += new System.EventHandler(this.odlbl_Click);
+            // 
+            // odTxtBox
+            // 
+            this.odTxtBox.Location = new System.Drawing.Point(115, 19);
+            this.odTxtBox.Name = "odTxtBox";
+            this.odTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.odTxtBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // idTxtBox
+            // 
+            this.idTxtBox.Location = new System.Drawing.Point(115, 60);
+            this.idTxtBox.Name = "idTxtBox";
+            this.idTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.idTxtBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Thickness";
+            // 
+            // thicknessTxtBox
+            // 
+            this.thicknessTxtBox.Location = new System.Drawing.Point(115, 95);
+            this.thicknessTxtBox.Name = "thicknessTxtBox";
+            this.thicknessTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.thicknessTxtBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Creator";
+            // 
+            // creatorTxtBox
+            // 
+            this.creatorTxtBox.Location = new System.Drawing.Point(115, 132);
+            this.creatorTxtBox.Name = "creatorTxtBox";
+            this.creatorTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.creatorTxtBox.TabIndex = 8;
+            // 
+            // mainWindowFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 218);
+            this.Controls.Add(this.creatorTxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.thicknessTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.idTxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.odTxtBox);
+            this.Controls.Add(this.odlbl);
+            this.Controls.Add(this.createBtn);
+            this.Name = "mainWindowFrm";
+            this.Text = "Sketch Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.Label odlbl;
+        private System.Windows.Forms.TextBox odTxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox idTxtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox thicknessTxtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox creatorTxtBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace DadProgram
+{
+    public partial class mainWindowFrm : Form
+    {
+        public mainWindowFrm()
+        {
+            InitializeComponent();
+            creatorTxtBox.Text = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split("\\")[1];
+        }
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            if (!validate_parameters())
+            {
+                MessageBox.Show("You need to fill out all of the parameters.", "Error");
+            } else {
+
+                Excel.Application excel = new Excel.Application();
+                Excel.Workbook workbook = excel.Workbooks.Open("D:\\Code\\VB\\VB_JSON_Reader\\test.xlsx", Type.Missing, true);
+                Excel._Worksheet worksheet = workbook.ActiveSheet;
+
+                Double OD = double.Parse(odTxtBox.Text);
+                int ID = int.Parse(idTxtBox.Text);
+                Double thickness = double.Parse(thicknessTxtBox.Text);
+
+
+
+                for (int i = 1; i < worksheet.UsedRange.Rows.Count - 1; i++)
+                {
+                    MessageBox.Show("OD: ");
+                    try
+                    {
+                        
+                        if (OD == double.Parse(worksheet.Range["A"+i].Value2))
+                        {
+                            if (ID == int.Parse(worksheet.Range["B"+1].Value2))
+                            {
+                                if (thickness == double.Parse(worksheet.Range["C"+i].Value2))
+                                {
+                                    MessageBox.Show("No sketch was found, Creating a new one.", "New Sketch");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Found sketch. Sketch number: " + worksheet.Range["D" + i].Value2, "Found Sketch");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Found sketch. Sketch number: " + worksheet.Range["D" + i].Value2, "Found Sketch");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Found sketch. Sketch number: " + worksheet.Range["D" + i].Value2, "Found Sketch");
+                        }
+                    } catch (Exception error)
+                    {
+                        Console.WriteLine(error);
+                    }
+                }
+
+            }       
+        }
+
+        private Boolean validate_parameters()
+        {
+            if (odTxtBox.Text == "") {
+                return false;
+            } else if (idTxtBox.Text == "") {
+                return false;
+            } else if (thicknessTxtBox.Text == "") {
+                return false;
+            } else if (creatorTxtBox.Text == "") {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        private void odlbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
